d680425e66fa38336d41237c51a80d2894764f02
Adam Lang <c.adam.lang@zonarsystems.com>
2019-07-18 19:22:37 -0700

    removed monitor junk

--- .history/app_20190718185427.js
+++ .history/app_20190718185427.js
@@ -2,15 +2,20 @@ const appRoot = require('app-root-path');
 require('dotenv').config({path: `${appRoot}/.env`});
 
 const spawn = require('child_process').spawn;
+const path = require('path');
 const fs = require('fs');
 const rp = require('request-promise');
 const os = require('os');
+const util = require('util');
 const moment = require('moment');
 const Promise = require('bluebird');
 const zlib = require('zlib')
+
 const ignore = require(`${appRoot}/config/ignore`);
 const db = require(`${appRoot}/config/db`);
 const s3Client = require(`${appRoot}/config/s3`);
+
+const monitor = process.argv[2] === '-nomonitor' ? false : true;  // determine if using CronAlarm
 const allDatabases = []; // will hold the name of each databases to be backed up
 
 //For sending results to CronAlarm
@@ -22,8 +27,14 @@ const formData = {
 };
 
 // If using CronAlarm, call the start API
-db.queryAsync('show databases')
+// Otherwise, start the promise chain with Promise.resolve()
+(monitor ? rp(`http://api.cronalarm.com/v2/${process.env.CRONALARM_KEY}/start`) : Promise.resolve())
+	.then(() => db.queryAsync('show databases'))
 	.then((dbs) => {
+		/*
+    Create an array of promises that will create a compressed
+			backup file for each database
+		*/
 		let promises = dbs.map((db) => {
 			if (ignore.indexOf(db.Database) === -1) { // make sure database is not in the ignore list
 				allDatabases.push(db.Database); // to be used later
@@ -77,6 +88,14 @@ db.queryAsync('show databases')
 		console.error(`backup failed with: ${e}`)
 	})
 	.finally(() => {
-		console.log('All done');
-		process.exit()
+		if (monitor) {
+			return rp({
+				method: 'POST',
+				uri: `http://api.cronalarm.com/v2/${process.env.CRONALARM_KEY}/end`,
+				form: formData
+			})
+		}
+
+		return Promise.resolve();
 	})
+	.then(() => process.exit());
